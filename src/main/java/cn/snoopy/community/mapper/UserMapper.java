72dbe5c16032bb2bef53b120f63e81b23b1fe5e7
snoopycode <snoopycodeemail.com>
2020-01-24 22:34:57 +0800

    login is ok

--- src/main/java/cn/snoopy/community/mapper/UserMapper.java
+++ src/main/java/cn/snoopy/community/mapper/UserMapper.java
@@ -6,6 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values(#{name},#{account_id},#{token},#{gmt_create},#{gmt_modified})")
+    @Insert("insert into user (name,accountId,token,gmtCreate,gmtModified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     public void insert(User user);
 }
